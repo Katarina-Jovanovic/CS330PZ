ea788a429cfa916e90eeaa89a123b8c4fe2dd5e6
Katarina-Jovanovic <katarina.jovanovic.4556@metropolitan.ac.rs>
2023-06-22 12:16:02 +0200

    commit 1

---
+++ app/src/main/java/rs/ac/metropolitan/cs330_pz/MainActivity.kt
@@ -0,0 +1,46 @@
+package rs.ac.metropolitan.cs330_pz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import rs.ac.metropolitan.cs330_pz.ui.theme.CS330PZTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CS330PZTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CS330PZTheme {
+        Greeting("Android")
+    }
+}
